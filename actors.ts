4c1a4e7901c7cb407774d08b3ad2469c24e7b7a3
Dayanna Muriel <iiiday.ashley@gmail.com>
2022-05-19 08:49:23 -0400

    actor

---
+++ actors.ts
@@ -0,0 +1,17 @@
+class Actor{
+    x : number;
+    y : number;
+
+    constructor(x : number, y : number) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Bird extends Actor{
+
+}
+
+class Pipes extends Actor{
+    
+}
