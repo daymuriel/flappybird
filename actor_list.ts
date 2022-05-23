21ae76e5e490d01b4b24d7d5b721c6bcf6031571
Dayanna Muriel <iiiday.ashley@gmail.com>
2022-05-23 08:31:50 -0400

    bird

--- actor_list.ts
+++ actor_list.ts
@@ -5,10 +5,6 @@ class ActorList {
         this.actors = [];
     }
 
-    /**
-     * Add actor to list (if not already included)
-     * @param actor 
-     */
     addActor(actor : Actor) : void {
         if (! this.actors.includes(actor)){
             this.actors.push(actor);
