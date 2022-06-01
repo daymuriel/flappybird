f86b5f04161eec8a625a65508ac793865a43fba0
Dayanna Muriel <iiiday.ashley@gmail.com>
2022-06-01 08:16:52 -0400

    pipes

--- event_handlers.js
+++ event_handlers.js
@@ -4,7 +4,6 @@ window.addEventListener("load", function () {
     actorlist.addActor(bird);
     let setpipe = new Pipes(0, 0, 0, 400);
     actorlist.addActor(setpipe);
-    canvas.style.background = "images/bg.jpg";
 });
 setInterval(function () {
     actorlist.addActor(new Pipes(400, 0, 400, 400));
